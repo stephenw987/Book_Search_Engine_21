c2d22becb0444252b18098ddbcd9911d39456537
Steve W <stephenw987@gmail.com>
2024-11-11 00:56:48 -0500

    5th push

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-// Import the ApolloServer class
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
-
-// Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
